96cfd08021b6e5c2cb89c3f1ba3d316d156b6441
Diego Beker <diegobecker2007@hotmail.com>
2023-04-22 12:56:11 -0300

    Add Signup and Signin forms

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,22 +1,74 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
-import MyWalletLogo from "../components/MyWalletLogo"
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import BASE_URL from "../constants/baseUrl";
+import UserContext from "../contexts/UserContext";
 
 export default function SignInPage() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      const unserializedData = JSON.parse(storedData);
+      setUser(unserializedData);
+      navigate("/home");
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function signIn(event) {
+    event.preventDefault();
+    axios
+      .post(`${BASE_URL}/sign-in`, form)
+      .then((response) => {
+        console.log(response);
+        const data = response.data;
+        const serializedData = JSON.stringify(data);
+        localStorage.setItem("userData", serializedData);
+        setUser(data);
+        navigate("/home");
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
         <button>Entrar</button>
       </form>
 
-      <Link>
-        Primeira vez? Cadastre-se!
-      </Link>
+      <Link to="/cadastro">Primeira vez? Cadastre-se!</Link>
     </SingInContainer>
-  )
+  );
 }
 
 const SingInContainer = styled.section`
@@ -25,4 +77,4 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,24 +1,85 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import MyWalletLogo from "../components/MyWalletLogo"
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import MyWalletLogo from "../components/MyWalletLogo";
+import { useState } from "react";
+import BASE_URL from "../constants/baseUrl";
+import axios from "axios";
 
 export default function SignUpPage() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const navigate = useNavigate();
+
+  function handleChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function signUp(event) {
+    event.preventDefault();
+    if (form.password === form.confirmPassword) {
+      const body = { ...form };
+      delete body.confirmPassword;
+      console.log(body);
+      console.log(BASE_URL);
+
+      axios
+        .post(`${BASE_URL}/sign-up`, body)
+        .then((response) => {
+          console.log(response);
+          navigate("/");
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Senhas não são iguais!");
+    }
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input
+          placeholder="Nome"
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          name="confirmPassword"
+          value={form.confirmPassword}
+          onChange={handleChange}
+          required
+        />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
-        Já tem uma conta? Entre agora!
-      </Link>
+      <Link to="/">Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
-  )
+  );
 }
 
 const SingUpContainer = styled.section`
@@ -27,4 +88,4 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
